ac11eb8a5d4aa273395de3d5cb0f01b09e5c8111
rkekesi22 <72697934+rkekesi22@users.noreply.github.com>
2026-03-24 14:50:46 +0100

    Added WPF UI

---
+++ SmartHomeUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using SmartHome.Controllers;
+using SmartHome.Devices;
+using SmartHome.Sensors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace SmartHomeUI.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly SmartHomeController _smartHomeController;
+        private readonly DispatcherTimer _timer;
+
+        private double _temperature;
+        public double Temperature
+        {
+            get => _temperature;
+            set
+            {
+                _temperature = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool _isHeating;
+        public bool IsHeating
+        {
+            get => _isHeating;
+            set
+            {
+                _isHeating = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _targetTemperature;
+        public double TargetTemperature
+        {
+            get => _targetTemperature;
+            set
+            {
+                _targetTemperature = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            _smartHomeController = new SmartHomeController();
+
+            _smartHomeController.AddSensor(new TemperatureSensor("TemperatureSensor 1"));
+            _smartHomeController.AddDevice(new Heater("Heater"));
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (s, e) => Update();
+            _timer.Start();
+        }
+
+        private void Update()
+        {
+            _smartHomeController?.Update();
+
+            var tempSensor = _smartHomeController.Sensors.OfType<TemperatureSensor>().FirstOrDefault();
+
+            if (tempSensor != null)
+            {
+                Temperature = tempSensor.ReadValue();
+            }
+
+            var heater = _smartHomeController.Devices.OfType<Heater>().FirstOrDefault();
+            if (heater != null)
+            {
+                IsHeating = heater.IsOn;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
